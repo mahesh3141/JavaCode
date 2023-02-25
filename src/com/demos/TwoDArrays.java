e80f191a40d1216186de9d4668f82d3ef7223811
mahesh3141 <nawale.mahesh85@gmail.com>
2023-02-25 15:26:40 +0530

    Diagonal sum in 2D array

---
+++ src/com/demos/TwoDArrays.java
@@ -0,0 +1,35 @@
+package com.demos;
+
+import java.util.Iterator;
+
+public class TwoDArrays {
+
+	public static void main(String[] args) {
+		int [][]a = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 10 } };
+		
+		PrintDiagonalSum(a); 
+	}
+	
+	static void PrintDiagonalSum(int a[][]) {
+		int left=0,right=0;
+		for(int row=0;row<a.length;row++) {
+			for(int col=0;col<a.length;col++) {
+				// left diagonal
+				if(row==col) {
+					left += a[row][col];
+				}
+				// right diagonal
+				if((row+col)==(a.length-1)) {
+					right+=a[row][col];
+				}
+			}
+		}
+		
+		System.out.println("Left Diagonal sum "+left);
+		System.out.println("Right Diagonal sum "+right);
+	}
+
+}
