0a91bd7f9860d2220deb66189cbd346f3f0b93c6
mahesh3141 <nawale.mahesh85@gmail.com>
2023-02-26 11:00:36 +0530

    rename the class

--- src/com/demos/DiagonalSumIn2DArray.java
+++ src/com/demos/DiagonalSumIn2DArray.java
@@ -2,7 +2,7 @@ package com.demos;
 
 import java.util.Iterator;
 
-public class TwoDArrays {
+public class DiagonalSumIn2DArray {
 
 	public static void main(String[] args) {
 		int [][]a = { { 1, 2, 3, 4 },
